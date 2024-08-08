206abb65f659e44256d7bfd1c4077d522880f435
Ebenezer Don <ebenezerdonu@gmail.com>
2024-08-08 12:41:07 +0100

    get prompt from request body

--- src/main.js
+++ src/main.js
@@ -1,25 +1,15 @@
 import fetch from 'node-fetch';
 import constants from './constants.js';
 
-// This is your Appwrite function
-// It's executed each time we get a request
 export default async ({ req, res, log, error }) => {
-  // You can log messages to the console
-  log('Hello, Logs!');
-
-  error('Hello, Errors!');
-
   const GEMINI_API_KEY = constants.GEMINI_API_KEY;
-  log('GEMINI_API_KEY ==>', GEMINI_API_KEY);
+
+  const body = req.body;
+  const prompt = body.prompt || 'Say something nice';
 
   const url =
     'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=' +
     GEMINI_API_KEY;
-
-  /*   curl \
-  -H 'Content-Type: application/json' \
-  -d '{"contents":[{"parts":[{"text":"Explain how AI works"}]}]}' \
-  -X POST 'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=YOUR_API_KEY' */
 
   const response = await fetch(url, {
     method: 'POST',
@@ -31,7 +21,7 @@ export default async ({ req, res, log, error }) => {
         {
           parts: [
             {
-              text: 'Say something nice',
+              text: prompt,
             },
           ],
         },
